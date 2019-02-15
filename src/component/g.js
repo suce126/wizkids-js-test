9080ddf5aac35a5d52554bdd4ede21ff3ea4769c
suce126 <suce126@126.com>
2019-02-15 20:27:46 +0100

    -g.js

--- src/component/g.js
+++
@@ -1,8 +0,0 @@
-const data = ["John", "Johnson", "Johnny", "Johann", "Johan", "Johnston", "Johannesburg", "Johanna", "Johns", "Johnstone", "Joh", "Johannes", "Johnnie", "Johansson", "Johnno", "Johnathan", "Johnsons", "Johal", "Johar"]
-
-for (let index = 0; index < 10; index++) {
-    const element = data[index]; 
-    console.log(element);
-}
-
-
